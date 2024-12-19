a1381d438f7aee807f8703f2821238c5eee597c3
John Lenin <64919951+J0hnLenin@users.noreply.github.com>
2024-12-19 22:07:39 +0500

    cards

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -23,20 +23,15 @@
 <div class="container text-center">
 @yield('body')
 </div>
-<footer class="footer">
-    <div class="footer-item">
-        <div class="footer-left">
-            <div>
-                <p class="author">Жилин Андрей</p>
-            </div>
-        </div>
+<footer>
+    <div class="footer-left">
+        <button class="btn btn-light" id="author" data-bs-toggle="popover" data-bs-title="Hey! I'm author of this site" data-bs-content="And I am a 3rd year student at Tyumen State University" >Andrey Zimin</button>
     </div>
-    <div class="footer-item">
-        <div class="footer-right">
-            <div class="icons">
-                <a href="https://web.telegram.org/SideArket"><img class="icon-image" alt="Andrey Zhilin"
-                                                                  src="https://cdn1.iconfinder.com/data/icons/unicons-line-vol-6/24/telegram-1024.png"></a>
-            </div>
+    <div class="footer-right">
+        <div class="icons">
+            <a href="https://letsgoplaygo.ru"><img class="icon-image" alt="Lets go play go" src="https://cdn1.iconfinder.com/data/icons/business-people-33/32/business_human_man_people_profile_user-512.png"></a>
+            <a href="https://github.com"><img class="icon-image" alt="GitHub" src="https://cdn4.iconfinder.com/data/icons/social-media-and-logos-12/32/Logo_Github-512.png"></a>
+            <a href="https://web.telegram.org"><img class="icon-image" alt="Telegram" src="https://cdn4.iconfinder.com/data/icons/social-media-and-logos-12/32/Logo_telegram_Airplane_Air_plane_paper_airplane-22-512.png"></a>
         </div>
     </div>
 </footer>
